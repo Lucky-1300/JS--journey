d8ab6929a029034e1e8822950d5e7726f53ac360
Lucky Ray <luckykumari42774@gmail.com>
2026-08-05 17:04:35 +0530

    Solve JavaScript explicit conversion, coercion, truthy/falsy, arithmetic and assignment operator tasks

---
+++ conversion.js
@@ -0,0 +1,335 @@
+// 1. Explicit Conversion (Number(), String(), Boolean())
+// Task 1
+// Convert "100" into a number and print its type.
+
+// let str = "100";
+// let num = Number(str);
+
+// console.log(num);
+// console.log(typeof num);
+
+// Task 2
+// Convert 250 into a string and print the value and type.
+// let num = 250;
+// let str = String(num);
+
+// console.log(str);
+// console.log(typeof str)
+
+// Task 3
+// Convert 0 into a boolean.
+
+// let num = 0;
+// let zero = Boolean(num);
+
+// console.log(zero)
+// console.log(typeof zero)
+
+// Task 4
+// Convert "Hello" into a boolean.
+
+// let str = "Hello";
+// let str2 = Boolean(str);
+
+// console.log(str2);
+// console.log(typeof str2);
+
+// Task 5
+// Convert "25.5" into a number and multiply it by 2.
+// let num = "25.5";
+// let num2 = Number(num);
+// console.log(num2);
+// console.log(typeof num2);
+// console.log(num2 * 2)
+
+// Task 6
+// Convert false into a number.
+
+// let num = false;
+// let num2 = Number(num)
+
+// console.log(num2)
+// console.log(typeof num2)
+
+// Task 7
+// Convert true into a string.
+// let num = true;
+// let num2 = String(num);
+
+// console.log(num2);
+// console.log(typeof num2);
+
+
+
+// Task 8
+// Convert null into a number.
+
+// let num = null;
+// let num2 = Number(num);
+
+// console.log(num2);
+// console.log(typeof num2);
+
+// Task 9
+// Convert undefined into a number.
+
+// let num = undefined;
+// let num2 = Number(num);
+
+// console.log(num2);
+// console.log(typeof num2);
+
+// Task 10
+// Convert an empty string "" into a boolean.
+
+// let str = "";
+// let str2 = Boolean(str);
+// console.log(str2);
+// console.log(typeof str2);
+
+
+// 2. Implicit Coercion
+// Predict the output.
+// Task 11
+// console.log("5" + 2);
+
+
+// Task 12
+// console.log("5" - 2);
+
+
+// Task 13
+// console.log("10" * 3);
+
+
+// Task 14
+// console.log("20" / 4);
+
+
+// Task 15
+// console.log("Hello" + 5);
+
+
+// Task 16
+// console.log("10" - "5");
+
+
+// Task 17
+// console.log(true + 2);
+
+
+// Task 18
+// console.log(false + false);
+
+
+// Task 19
+// console.log(null + 5);
+
+
+// Task 20
+// console.log(undefined + 5);
+
+// 3. Truthy & Falsy Values
+// Write whether the condition executes.
+// Task 21
+// if ("Hello") {
+//     console.log("Yes");
+// }
+
+
+// Task 22
+// if (0) {
+//     console.log("Hello");
+// }
+
+
+// Task 23
+// if ("") {
+//     console.log("Hi");
+// }
+
+
+// Task 24
+// if ([]) {
+//     console.log("Array");
+// }
+
+
+// Task 25
+// if ({}) {
+//     console.log("Object");
+// }
+
+
+// Task 26
+// if (NaN) {
+//     console.log("NaN");
+// }
+
+
+// Task 27
+// if ("0") {
+//     console.log("String Zero");
+// }
+
+
+// Task 28
+// if (-10) {
+//     console.log("Negative");
+// }
+
+
+// Task 29
+// if (undefined) {
+//     console.log("Undefined");
+// }
+
+
+// Task 30
+// if (null) {
+//     console.log("Null");
+// }
+
+// 4. Arithmetic Operators
+// Task 31
+// Find the result.
+// 15 + 10
+
+// console.log(15 + 10)
+// console.log(20 - 7)
+
+
+// Task 32
+// 20 - 7
+
+
+
+// Task 33
+// 6 * 8
+// console.log(6 * 8)
+
+
+// Task 34
+// 20 / 5
+// console.log(20/5)
+
+
+// Task 35
+// 25 % 4
+
+// console.log(25 % 4)
+
+
+// Task 36
+// 2 ** 5
+// console.log(2 ** 5)
+
+
+// Task 37
+// Calculate the average of three numbers.
+
+// let num1 = 10;
+// let num2 = 20;
+// let num3 = 30;
+
+// let average = (num1 + num2 + num3) / 3
+// console.log(average)
+
+// Task 38
+// Swap two numbers using arithmetic operators.
+// let num1 = 30;
+// let num2 = 31;
+
+// num1 = num1 + num2;
+// num2 = num1 - num2;
+// num1 = num1 - num2;
+// console.log(num1);
+// console.log(num2)
+
+
+// Task 39
+// Find whether a number is even or odd using %.
+
+// let num = 15;
+
+// if(num % 2 === 0){
+//    console.log("even")
+// }else{
+//     console.log("odd")
+// };
+
+// Task 40
+// Find the square of a number.
+
+
+// let num = 18;
+// let square = num * num;
+// console.log(square)
+
+
+
+
+// 5. Assignment Operators
+// Task 41
+// let x = 10;
+// x += 5;
+// console.log(x)
+
+
+// Task 42
+// let x = 20;
+// x -= 7;
+// console.log(x)
+
+
+// Task 43
+// let x = 6;
+// x *= 3;
+// console.log(x)
+
+
+
+// Task 44
+// let x = 20;
+// x /= 4;
+// console.log(x)
+
+
+// Task 45
+// let x = 15;
+// x %= 4;
+// console.log(x)
+
+
+// Task 46
+// let x = 2;
+// x **= 4;
+// console.log(x)
+
+
+// Task 47
+// Update salary by adding bonus.
+// let salary = 15000;
+// salary += 12000;
+// console.log(salary)
+
+
+// Task 48
+// Reduce account balance after shopping.
+// let amount = 15000;
+// amount -= 12000;
+// console.log(amount)
+
+// Task 49
+// Multiply marks by 2.
+// let marks = 45;
+// marks *= 2;
+// console.log(marks)
+
+// Task 50
+// Divide total bill equally among 4 friends.
+
+// let bill = 2000;
+// bill /= 4;
+// console.log(bill)
+
