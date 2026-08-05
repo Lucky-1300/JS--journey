1eef20d81bc24e97ea89fecc8bb4b448a0589d2f
Lucky Ray <luckykumari42774@gmail.com>
2026-08-05 15:27:57 +0530

    done some variable task

---
+++ variable.js
@@ -0,0 +1,240 @@
+// task 1
+
+/*
+Using const
+const constName = "Lucky";
+const constAge = 20;
+const constCountry = "India";
+
+Using let
+let letName = "Lucky";
+let letAge = 20;
+let letCountry = "India";
+
+Using var
+var varName = "Lucky";
+var varAge = 20;
+var varCountry = "India";
+
+*/
+
+
+/*
+Task 2: Declaration vs Initialization
+Declare a variable without assigning a value.
+Print it.
+Assign a value later.
+Print it again.
+
+let name;
+console.log(name)
+
+
+name = "Lucky";
+console.log(name)
+
+*/
+
+
+/*
+Task 3: Reassignment
+Create a variable using let.
+Store your favorite color.
+Change it to another color.
+Print both values.
+
+let color = "black"
+console.log(color)
+
+color = "pink"
+console.log(color)
+
+*/
+
+
+/*
+Task 4: Const Variable
+Create a constant named PI.
+Try changing its value.
+Observe the error.
+
+const name = "Lucky"
+console.log(name)
+name = "honey"
+
+console.log(name)
+
+*/
+
+
+// Task 5: Redeclaration
+// Try this:
+// let age = 20;
+// let age = 25;
+
+// What happens?
+// Now try with var.
+
+// let age = 20;
+// let age = 25;
+
+// console.log(age)
+
+// var age = 20;
+// var age = 25;
+// console.log(age)
+
+
+// Task 6: Valid Variable Names
+// Create variables named:
+// firstName
+// last_name
+// $price
+// _id
+// user123
+// Print them.
+
+// const firstName = "Lucky";
+// const last_name = "Honey";
+// const $price = 100;
+// const _id = 1;
+// const user123 = "Lucky123";
+
+
+// console.log(firstName);
+// console.log(last_name);
+// console.log($price);
+// console.log(_id);
+// console.log(user123);
+
+
+// let 123name = "Lucky";
+// let first-name = "ray";
+// let let;
+
+
+
+// Task 8: Rename Variables
+// Convert these into proper camelCase:
+// student name
+// user age
+// phone number
+// total marks
+
+
+// let studentName = "Lucky";
+// let userName = "lucky089";
+// let userAge = 20;
+// let phoneNumber = 7979950146;
+// let totalMarks = 50;
+
+
+// Task 9: Personal Information
+// Store:
+// Name
+// Age
+// Gender
+// City
+// Is Student
+// Print them in one sentence.
+// Example:
+// My name is Lucky.
+// I am 22 years old.
+
+/*
+const name = "Lucky";
+const age = 20;
+const gender = "female";
+const city = "Bokaro";
+const isStudent = true;
+
+console.log("My name is ",name)
+console.log("I am ",age , "years old")
+*/
+
+// Task 10: Student Record
+// Create variables for:
+// Roll Number
+// Marks
+// Grade
+// Passed
+// Print everything.
+
+/*
+
+let rollNumber = 22;
+let marks = 44;
+let grade = "B";
+let passed = true;
+
+console.log(rollNumber);
+console.log(marks);
+console.log(grade);
+console.log(passed);
+
+*/
+
+// Level 3: Primitive Data Types
+// Task 11: Identify Data Types
+// Store:
+// "Hello"
+// 25
+// true
+// undefined
+// null
+
+// Print each variable with its typeof.
+
+// console.log(typeof("Hello"));
+// console.log(typeof(25));
+// console.log(typeof(true));
+// console.log(typeof(undefined));
+// console.log(typeof(null));
+
+
+
+// Task 12: String Practice
+// Create variables for:
+// First Name
+// Last Name
+// Combine them into Full Name.
+
+// Task 13: Number Operations
+// Store:
+// 50
+// 20
+
+// Print:
+// Sum
+// Difference
+// Product
+// Division
+
+// Task 14: Boolean Example
+// Create variables:
+// isLoggedIn
+// isAdmin
+
+// Print both.
+
+// Task 15: Undefined
+// Declare:
+// let address;
+
+// Print it.
+// Then assign a value.
+// Print again.
+
+// Task 16: Null
+// Create:
+// let selectedUser = null;
+
+// Later assign:
+// "Lucky"
+
+// Print both.
+
+
+
+
+
